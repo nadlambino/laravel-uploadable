ce4619a614c8ff308a58debcd649adc67e2138dc
Ronald Lambino <50892397+nadlambino@users.noreply.github.com>
2024-03-17 21:08:55 +0800

    feat: add url method

--- src/Uploadable.php
+++ src/Uploadable.php
@@ -10,9 +10,12 @@ class Uploadable implements UploadableContract
 {
     private string $basePath;
 
+    private object $config;
+
     public function __construct(private readonly Filesystem $storage)
     {
-        $this->basePath = trim(app('uploadable')->path, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
+        $this->config = app('uploadable');
+        $this->basePath = trim($this->config->path, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
     }
 
     public function upload(UploadedFile $file, ?string $path = null, ?string $name = null) : ?string
@@ -21,5 +24,18 @@ class Uploadable implements UploadableContract
         $name = $name ?? $file->hashName();
 
         return $this->storage->putFileAs($path, $file, $name);
+    }
+
+    public function url(string $file) : ?string
+    {
+        $url = $this->storage->url(trim($file, DIRECTORY_SEPARATOR));
+
+        if (! isset($this->config->host)) {
+            return $url;
+        }
+
+        $path = parse_url($url, PHP_URL_PATH);
+
+        return $this->config->host . $path;
     }
 }
