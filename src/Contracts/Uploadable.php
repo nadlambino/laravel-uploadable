ad185c0259ac6bd700fd67d932504c2724d3c6f0
Ronald Lambino <50892397+nadlambino@users.noreply.github.com>
2024-03-17 21:30:34 +0800

    feat: add get method

--- src/Contracts/Uploadable.php
+++ src/Contracts/Uploadable.php
@@ -8,6 +8,8 @@ interface Uploadable
 {
     public function upload(UploadedFile $file, ?string $path = null, ?string $name = null): ?string;
 
+    public function get(string $file): ?string;
+
     public function url(string $file): ?string;
 
     public function temporaryUrl(string $file, int $expiration = 60, array $options = []): ?string;
