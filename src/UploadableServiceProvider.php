15149406ce0ccf1331d6418229ff9700b5dbe0a8
Ronald Lambino <50892397+nadlambino@users.noreply.github.com>
2024-03-18 12:29:09 +0800

    feat: create uploads data and upload file from request

--- src/UploadableServiceProvider.php
+++ src/UploadableServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace NadLambino\Uploadable;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Storage;
+use NadLambino\Uploadable\Models\Traits\HasFile;
+use NadLambino\Uploadable\Observers\UploadableObserver;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
@@ -11,15 +15,10 @@ class UploadableServiceProvider extends PackageServiceProvider
 {
     public function boot() : void
     {
-        parent::boot();
-
-        $this->app->bind('uploadable', function () {
-            return (object) config('uploadable.env.'.app()->environment());
-        });
-
-        $this->app->bind(\NadLambino\Uploadable\Contracts\Uploadable::class, function ($app) {
-            return new Uploadable(Storage::disk($app['uploadable']->disk));
-        });
+        parent::boot()
+            ->bindConfig()
+            ->bindService()
+            ->observeModel();
     }
 
     public function configurePackage(Package $package): void
@@ -34,5 +33,41 @@ class UploadableServiceProvider extends PackageServiceProvider
                     ->publishMigrations()
                     ->copyAndRegisterServiceProviderInApp();
             });
+    }
+
+    protected function bindConfig(): static
+    {
+        $this->app->bind('uploadable', function () {
+            return (object) config('uploadable.env.'.app()->environment());
+        });
+
+        return $this;
+    }
+
+    protected function bindService(): static
+    {
+        $this->app->bind(\NadLambino\Uploadable\Contracts\Uploadable::class, function ($app) {
+            return new Uploadable(Storage::disk($app['uploadable']->disk));
+        });
+
+        $this->app->bind(Uploadable::class, function ($app) {
+            return new Uploadable(Storage::disk($app['uploadable']->disk));
+        });
+
+        return $this;
+    }
+
+    protected function observeModel(): static
+    {
+        Event::listen('eloquent.booted: *', function ($eventName, array $data) {
+            /** @var Model $model */
+            $model = $data[0];
+
+            if (in_array(HasFile::class, class_uses_recursive($model))) {
+                $model::observe(UploadableObserver::class);
+            }
+        });
+
+        return $this;
     }
 }
