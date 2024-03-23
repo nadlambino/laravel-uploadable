f15efefa38f4be66d858485145ffabef128bba21
Ronald Lambino <50892397+nadlambino@users.noreply.github.com>
2024-03-23 16:36:20 +0800

    feat: file validation

--- src/Models/Traits/HasFile.php
+++ src/Models/Traits/HasFile.php
@@ -5,6 +5,7 @@ namespace NadLambino\Uploadable\Models\Traits;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\Relations\MorphOne;
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Str;
 use NadLambino\Uploadable\Contracts\Uploadable;
 use NadLambino\Uploadable\Models\Upload;
 use NadLambino\Uploadable\Observers\UploadableObserver;
@@ -32,18 +33,33 @@ trait HasFile
         return $this->morphMany(Upload::class, 'uploadable');
     }
 
-    public function setFileUpload(UploadedFile $file) : static
+    protected function getUploadableRequestName() : string
     {
+        return Str::of(__TRAIT__)->classBasename()->replace('Has', '')->lower();
+    }
+
+    protected function getUploadableRequestRules() : array
+    {
+        return [
+            'sometimes',
+            'file'
+        ];
+    }
+
+    public function setUploadable(UploadedFile $file) : static
+    {
+        request()->validate([
+            $this->getUploadableRequestName() => $this->getUploadableRequestRules(),
+        ]);
+
         $this->uploadedFile = $file;
 
         return $this;
     }
 
-    public function getFileUpload() : ?UploadedFile
+    public function getUploadable() : ?UploadedFile
     {
-        if (is_null($this->file)) {
-            $this->uploadedFile = request()->file('file');
-        }
+        $this->setUploadable(request()->file($this->getUploadableRequestName()));
 
         return $this->uploadedFile;
     }
