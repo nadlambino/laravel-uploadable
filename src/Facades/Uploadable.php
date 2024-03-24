1caee019acd71f71ec4367f6021556b715a599f4
Ronald Lambino <50892397+nadlambino@users.noreply.github.com>
2024-03-24 19:21:52 +0800

    chore: add basic tests

--- src/Facades/Uploadable.php
+++ src/Facades/Uploadable.php
@@ -6,6 +6,13 @@ use Illuminate\Support\Facades\Facade;
 
 /**
  * @see \NadLambino\Uploadable\Uploadable
+ *
+ * @method static string|null upload(\Illuminate\Http\UploadedFile $file, ?string $path = null, ?string $name = null) Uploads a file and optionally stores it at a given path with a given name.
+ * @method static string|null get(string $file) Retrieves the contents of a file.
+ * @method static string|null url(string $file) Retrieves the URL of a file.
+ * @method static string|null temporaryUrl(string $file, int $expiration = 60, array $options = []) Retrieves a temporary URL for a file, which expires after a given number of minutes.
+ * @method static bool delete(string $file) Deletes a file.
+ * @method static bool exists(string $file) Checks if a file exists.
  */
 class Uploadable extends Facade
 {
