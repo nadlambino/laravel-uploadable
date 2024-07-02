aa15115f39712465fd379833270d8bdb50109be2
Ronald Lambino <50892397+nadlambino@users.noreply.github.com>
2024-07-02 16:44:02 +0800

    test: create a function that do the uploads

--- tests/UploadActionTest.php
+++ tests/UploadActionTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\UploadedFile;
 use NadLambino\Uploadable\Actions\Upload;
 use NadLambino\Uploadable\Models\Upload as ModelsUpload;
@@ -7,44 +8,46 @@ use NadLambino\Uploadable\Tests\Models\TestPost;
 use NadLambino\Uploadable\Tests\Models\TestPostWithCustomFilename;
 use NadLambino\Uploadable\Tests\Models\TestPostWithCustomPath;
 
+function uploadFileFor(Model $model, array|UploadedFile $files = null, array $options = [])
+{
+    if ($files === null) {
+        $files = UploadedFile::fake()->image('avatar.jpg');
+    }
+
+    /** @var Upload $action */
+    $action = app(Upload::class);
+    $action->handle($files, $model, $options);
+}
+
 it('can upload a file for a given model', function () {
-    $file = UploadedFile::fake()->image('avatar.jpg');
     $post = new TestPost();
     $post->title = fake()->sentence();
     $post->body = fake()->paragraph();
     $post->save();
 
-    /** @var Upload $action */
-    $action = app(Upload::class);
-    $action->handle($file, $post);
+    uploadFileFor($post);
 
     expect($post->uploads()->first())->not->toBeNull();
 });
 
 it('can upload a file for a given model with custom filename', function () {
-    $file = UploadedFile::fake()->image('avatar.jpg');
     $post = new TestPostWithCustomFilename();
     $post->title = fake()->sentence();
     $post->body = fake()->paragraph();
     $post->save();
 
-    /** @var Upload $action */
-    $action = app(Upload::class);
-    $action->handle($file, $post);
+    uploadFileFor($post, $file = UploadedFile::fake()->image('avatar.jpg'));
 
     expect($post->uploads()->first()->name)->toBe($file->getClientOriginalName());
 });
 
 it('can upload a file for a given model with custom path', function () {
-    $file = UploadedFile::fake()->image('avatar.jpg');
     $post = new TestPostWithCustomPath();
     $post->title = fake()->sentence();
     $post->body = fake()->paragraph();
     $post->save();
 
-    /** @var Upload $action */
-    $action = app(Upload::class);
-    $action->handle($file, $post);
+    uploadFileFor($post);
 
     expect($post->uploads()->first()->path)->toContain('custom_path');
 });
@@ -54,15 +57,13 @@ it('can upload a file and save the upload with additional data using the `before
     TestPost::beforeSavingUploadUsing(function (ModelsUpload $upload) use ($tag) {
         $upload->tag = $tag;
     });
+
     $post = new TestPost();
     $post->title = fake()->sentence();
     $post->body = fake()->paragraph();
     $post->save();
 
-    $file = UploadedFile::fake()->image('avatar.jpg');
-    /** @var Upload $action */
-    $action = app(Upload::class);
-    $action->handle($file, $post, ['before_saving_upload_using' => TestPost::$beforeSavingUploadCallback]);
+    uploadFileFor($post, options: ['before_saving_upload_using' => TestPost::$beforeSavingUploadCallback]);
 
     expect($post->uploads()->first()->tag)->toBe($tag);
 });
@@ -73,10 +74,7 @@ it('can upload a file and save the upload with additional data using the `before
     $post->body = fake()->paragraph();
     $post->save();
 
-    $file = UploadedFile::fake()->image('avatar.jpg');
-    /** @var Upload $action */
-    $action = app(Upload::class);
-    $action->handle($file, $post);
+    uploadFileFor($post);
 
     expect($post->uploads()->first()->tag)->toBe($post->title);
 });
