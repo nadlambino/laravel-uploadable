367c5c336ccc9dee414e69643e09e67e1152037c
Ronald Lambino <50892397+nadlambino@users.noreply.github.com>
2024-07-03 13:26:59 +0800

    feat: add the ability to provide a single or an array of uploaded files

--- src/Concerns/Uploadable.php
+++ src/Concerns/Uploadable.php
@@ -8,6 +8,8 @@ trait Uploadable
 {
     use Options, Relations, Validation;
 
+    protected array $uploadFrom = [];
+
     public static function bootUploadable(): void
     {
         static::replacePreviousUploads(config('uploadable.replace_previous_uploads', false));
@@ -27,6 +29,10 @@ trait Uploadable
 
     public function getUploads(): array
     {
+        if (! empty($this->uploadFrom)) {
+            return $this->uploadFrom;
+        }
+
         $rules = $this->getUploadRules();
 
         /** @var \Illuminate\Http\Request $request */
@@ -44,5 +50,24 @@ trait Uploadable
             ->all();
 
         return $request->only($fields);
+    }
+
+    /**
+     * Set the files that should be uploaded. This can be use on a Livewire
+     * component where the request object doesn't contain the UploadedFiles.
+     * Note that the validation will not be perform on these files, so make
+     * sure that they are already validated. Livewire provides a way to do
+     * the validation by using Validation attributes
+     *
+     * @param array|UploadedFile $files The files to upload.
+     *                                  Can be a single or an array of files.
+     *                                  File can be an instance of Illuminate\Http\UploadedFile
+     *                                  or a full path to a file uploaded on the temporary disk.
+     *
+     * @return void
+     */
+    public function uploadFrom(array|UploadedFile|string $files): void
+    {
+        $this->uploadFrom = is_array($files) ? $files : [$files];
     }
 }
--- tests/UploadActionTest.php
+++ tests/UploadActionTest.php
@@ -451,4 +451,47 @@ it('should skip the validation for a specific uploadable model', function () {
     expect($post->uploads()->count())->toBe(1);
 });
 
+it('can upload a file outside of the context of request', function () {
+    $post = new TestPost();
+    $post->uploadFrom([
+        'images' => [
+            UploadedFile::fake()->image('avatar1.jpg'),
+            UploadedFile::fake()->image('avatar2.jpg'),
+        ],
+        'video' => UploadedFile::fake()->create('video.mp4', 1000, 'video/mp4'),
+    ]);
+    $post->title = fake()->sentence();
+    $post->body = fake()->paragraph();
+    $post->save();
+
+    $files = $post->getUploads();
+
+    upload_file_for($post, $files);
+
+    expect($post->uploads()->count())->toBe(3);
+
+    $uploads = $post->uploads()->get();
+
+    expect(Storage::exists($uploads[0]->path))->toBeTrue();
+    expect(Storage::exists($uploads[1]->path))->toBeTrue();
+    expect(Storage::exists($uploads[2]->path))->toBeTrue();
+});
+
+it('can upload a file outside of the context of request using a string path from temporary disk', function () {
+    $fullpath = UploadedFile::fake()->image('avatar.jpg')->store('tmp', config('uploadable.temporary_disk', 'local'));
+
+    $post = new TestPost();
+    $post->uploadFrom($fullpath);
+    $post->title = fake()->sentence();
+    $post->body = fake()->paragraph();
+    $post->save();
+
+    $files = $post->getUploads();
+
+    upload_file_for($post, $files);
+
+    expect($post->uploads()->count())->toBe(1);
+    expect(Storage::exists($post->uploads()->first()->path))->toBeTrue();
+});
+
 // TODO: test the deletion and rollback of uploadable model when an error occurs on queue
