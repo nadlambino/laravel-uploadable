714614e3964319d3af2eb894a7d007dd46866d1e
Ronald Lambino <50892397+nadlambino@users.noreply.github.com>
2024-07-03 13:57:16 +0800

    ref: make UploadOptions dto to be read-only yet writable within the class itself.

--- src/Dto/UploadOptions.php
+++ src/Dto/UploadOptions.php
@@ -4,19 +4,59 @@ namespace NadLambino\Uploadable\Dto;
 
 use Laravel\SerializableClosure\SerializableClosure;
 
-readonly class UploadOptions
+/**
+ * @property bool $deleteModelOnUploadFail
+ * @property bool $deleteModelOnQueueUploadFail
+ * @property bool $forceDeleteUploads
+ * @property bool $replacePreviousUploads
+ * @property bool $rollbackModelOnUploadFail
+ * @property bool $rollbackModelOnQueueUploadFail
+ * @property string|null $queue
+ * @property string $temporaryDisk
+ */
+class UploadOptions
 {
     public function __construct(
-        public ?SerializableClosure $beforeSavingUploadUsing = null,
-        public bool $deleteModelOnUploadFail = true,
-        public bool $deleteModelOnQueueUploadFail = false,
-        public bool $dontUpload = false,
-        public bool $forceDeleteUploads = false,
-        public array $originalAttributes = [],
-        public bool $replacePreviousUploads = false,
-        public bool $rollbackModelOnUploadFail = true,
-        public bool $rollbackModelOnQueueUploadFail = false,
-        public ?string $queue = null,
-        public string $temporaryDisk = 'local',
-    ) { }
+        public readonly ?SerializableClosure $beforeSavingUploadUsing = null,
+        public readonly bool $dontUpload = false,
+        public readonly array $originalAttributes = [],
+        private ?bool $deleteModelOnUploadFail = null,
+        private ?bool $deleteModelOnQueueUploadFail = null,
+        private ?bool $forceDeleteUploads = null,
+        private ?bool $replacePreviousUploads = null,
+        private ?bool $rollbackModelOnUploadFail = null,
+        private ?bool $rollbackModelOnQueueUploadFail = null,
+        private ?string $queue = null,
+        private ?string $temporaryDisk = null,
+    ) {
+        $this->deleteModelOnUploadFail ??= config('uploadable.delete_model_on_upload_fail', true);
+        $this->deleteModelOnQueueUploadFail ??= config('uploadable.delete_model_on_queue_upload_fail', false);
+        $this->forceDeleteUploads ??= config('uploadable.force_delete_uploads', false);
+        $this->replacePreviousUploads ??= config('uploadable.replace_previous_uploads', false);
+        $this->rollbackModelOnUploadFail ??= config('uploadable.rollback_model_on_upload_fail', true);
+        $this->rollbackModelOnQueueUploadFail ??= config('uploadable.rollback_model_on_queue_upload_fail', false);
+        $this->queue ??= config('uploadable.queue', null);
+        $this->temporaryDisk ??= config('uploadable.temporary_disk', 'local');
+    }
+
+    /**
+     * Make the properties read-only outside of this class
+     * yet allow them to be set in the constructor.
+     * This is to allow setting their default values
+     * based on config values.
+     *
+     * @param string $name
+     * @param mixed $value
+     *
+     * @throws \RuntimeException
+     */
+    public function __set($name, $value): void
+    {
+        throw new \RuntimeException(sprintf('Property %s is read-only', $name));
+    }
+
+    public function __get($name): mixed
+    {
+        return $this->$name;
+    }
 }
